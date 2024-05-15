d134953493d97c1b6c64276adc2063e89d5ebca3
Aditya Sai Kolluru <adityasaikolluru@Friday.local>
2024-05-15 18:19:13 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/My-ToDo-List-App/",
+  base: "/my-todo-app/",
+
 })
